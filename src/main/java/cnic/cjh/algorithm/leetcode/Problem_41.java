5edc7322c8662cab15e967fa8610c3699d29527e
blueCao <770541944@qq.com>
2018-01-26 00:54:04 +0800

    LeetCode 41 送分题，O(n)找出数组中第一个未出现过的最小正整数。标记法解决，减少二次遍历、减少字符拷贝、减少判断提高效率

--- src/main/java/cnic/cjh/algorithm/leetcode/Problem_41.java
+++ src/main/java/cnic/cjh/algorithm/leetcode/Problem_41.java
@@ -7,7 +7,7 @@ package cnic.cjh.algorithm.leetcode;
 public class Problem_41 {
     public int firstMissingPositive(int[] nums) {
         //用于标记出现过的数字,leble[0]=1,表示整数“1”出现过
-        int[] labels = new int[nums.length + 1];
+        byte[] labels = new byte[nums.length];
         for(int i = 0; i < nums.length; i++){
             if( nums[i] >  nums.length || nums[i] < 1){
                 //超出了范围 [1 , nums.length]
@@ -20,11 +20,15 @@ public class Problem_41 {
             }
         }
         //寻找第一个未被标记的位置，返回该位置对应的值
-        for(int i=0 ; i < labels.length; i++){
-            if(nums[i] != 1){
+        for(int i=0 ; i < nums.length; i++){
+            if(labels[i] != 1){
                 return i+1;
             }
         }
-        return -1;
+        //全部标记，返回下一位未出现过的整数
+        return nums.length + 1;
+    }
+    public static  void main(String[] args){
+        new Problem_41().firstMissingPositive(new int[]{});
     }
 }
