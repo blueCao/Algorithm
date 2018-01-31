1c453a92acc29a3462f09fe46c5862fe4d72655e
Ove <770541944@qq.com>
2018-01-31 16:43:33 +0800

    LeetCode 15 从数组中找出所有三个数之和为0的所有唯一的组合。超时！未解决！

--- src/main/java/cnic/cjh/algorithm/leetcode/Problem_15.java
+++ src/main/java/cnic/cjh/algorithm/leetcode/Problem_15.java
@@ -1,5 +1,7 @@
 package cnic.cjh.algorithm.leetcode;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.*;
 
 /**
@@ -10,27 +12,29 @@ import java.util.*;
 public class Problem_15 {
     public List<List<Integer>> threeSum(int[] nums) {
         //排序
+System.out.println(new Date());
         Arrays.sort(nums);
+System.out.println(new Date()+"sorted done!");
+        List<Integer> indexMap = buildIndexMap(nums);
+        List<Integer> valueMap = buildValueMap(nums);
+System.out.println(new Date()+"map build done!");
 
+        //保存值索引
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         for(int i=0; i < nums.length; i++){
+            if(nums[i] > 0) {
+                continue;
+            }
             List<Integer> tracks = new ArrayList<Integer>();
             tracks.add(i);
-            DFS(nums,tracks,3,result,nums[i]);
+            DFS(nums,tracks,3,result,nums[i],indexMap);
         }
-        //过滤
-        Set<Trible> set = new HashSet<Trible>(result.size());
-        for(int i=0; i<result.size(); i++){
-            set.add(new Trible(result.get(i)));
-        }
-        result.clear();
-        Iterator<Trible> i = set.iterator();
-        for(;i.hasNext();){
-            result.add(i.next().toList());
-        }
-        return result;
+System.out.println(new Date()+"DFS done!");
+        //过滤重复的结果索引
+        return bitmap(result,valueMap);
     }
-    private void DFS(int[] nums, List<Integer> tracks, int depth, List<List<Integer>> result, int sum){
+
+    private void DFS(int[] nums, List<Integer> tracks, int depth, List<List<Integer>> result, int sum, List<Integer> indexMap){
         if(tracks.size() < depth){
             //剪枝：和大于 0 停止查找，因为nums数组是非递减的排序
             if(sum > 0){
@@ -38,58 +42,96 @@ public class Problem_15 {
             }
             for (int i=tracks.get(tracks.size() - 1) + 1; i < nums.length; i++){
                 tracks.add(i);
-                DFS(nums,tracks,3,result,sum + nums[i]);
+                DFS(nums,tracks,3,result,sum + nums[i],indexMap);
                 tracks.remove(tracks.size() - 1);
             }
         }else{
-            //达到最大深度：停止查找，保留和 0 的结果
+            //达到最大深度：停止查找，保留和 0 的结果索引
             if (sum == 0){
                 List<Integer> r = new ArrayList<Integer>();
                 for(int i=0; i<depth; i++){
-                    r.add(nums[tracks.get(i)]);
+                    //保存值索引
+                    r.add(indexMap.get(tracks.get(i)));
                 }
                 result.add(r);
             }
         }
     }
 
-    class Trible{
-        int a;
-        int b;
-        int c;
-        public Trible(List<Integer> array){
-            if(array == null || array.size() < 3){
-                System.out.println("构造 Trible 错误：数组长度小于三");
-                return;
+    /**
+     * 根据输入的数组，构造一个每个数组位置对应的位置索引号
+     * @param nums
+     * @return
+     */
+    public List<Integer> buildIndexMap(int[] nums){
+        List<Integer> result = new ArrayList<Integer>();
+        if(nums != null && nums.length > 0){
+            int pre = nums[0],index=0;
+            result.add(0);
+            for(int i=1; i<nums.length; i++){
+                if(pre != nums[i]){
+                    index++;
+                    pre = nums[i];
+                }
+                result.add(index);
             }
-            int[] sorted = {array.get(0),array.get(1),array.get(2)};
-            //排序，依次赋值
-//            Arrays.sort(sorted);
-            a = sorted[0];
-            b = sorted[1];
-            c = sorted[2];
         }
-        public List<Integer> toList(){
-            List<Integer> l = new ArrayList<Integer>(3);
-            l.add(a);
-            l.add(b);
-            l.add(c);
-            return l;
-        }
+        return result;
+    }
 
-        @Override
-        public int hashCode() {
-            return Integer.toString(a).hashCode()+ Integer.toString(b).hashCode() + Integer.toString(c).hashCode();
-        }
 
-        @Override
-        public boolean equals(Object obj) {
-            Trible t = (Trible)obj;
-            return t.a == a && t.b == b && t.c == c;
+    /**
+     * 根据输入的数组，构造一个值索引
+     * @param nums
+     * @return
+     */
+    public List<Integer> buildValueMap(int[] nums){
+        List<Integer> result = new ArrayList<Integer>();
+        if(nums != null && nums.length > 0){
+            int pre = nums[0];
+            result.add(pre);
+            for(int i=1; i<nums.length; i++){
+                if(pre != nums[i]){
+                    result.add(nums[i]);
+                    pre = nums[i];
+                }
+            }
         }
+        return result;
+    }
+
+    /**
+     * 过滤掉重复的索引，返回去重的结果
+     * @param origin
+     * @return
+     */
+    public List<List<Integer>> bitmap(List<List<Integer>> origin,List<Integer> valueMap){
+        int[][][] map = new int[valueMap.size()][valueMap.size()][valueMap.size()];
+        List<Integer> tmp;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for(int i=0; i<origin.size(); i++){
+            tmp = origin.get(i);
+            if (tmp != null && tmp.size() > 2){
+                if(map[tmp.get(0)][tmp.get(1)][tmp.get(2)] == 0){
+                    map[tmp.get(0)][tmp.get(1)][tmp.get(2)] = 1;
+                    List<Integer> r = new ArrayList<Integer>(3);
+                    r.add(valueMap.get(tmp.get(0)));
+                    r.add(valueMap.get(tmp.get(1)));
+                    r.add(valueMap.get(tmp.get(2)));
+                    result.add(r);
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args){
-        System.out.println(new Problem_15().threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        int length = 100000;
+        int[] input = new int[length];
+        for(int i=-0; i<length; i++){
+            input[i] = (int)(Math.random() * length -length/2);
+        }
+        System.out.println(new Problem_15().threeSum(input));
     }
+
 }
