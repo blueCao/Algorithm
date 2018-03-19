20f4ba0d4547195b6ebd3f0476c6e8173b0c7c92
caojunhui <770541944@qq.com>
2018-03-19 09:08:56 +0800

    合并k个有序链表，利用优先队列达到了时间要求 O(n lgn)

--- src/main/java/cnic/cjh/algorithm/leetcode/Problem_24.java
+++ src/main/java/cnic/cjh/algorithm/leetcode/Problem_24.java
@@ -20,14 +20,14 @@ public class Problem_24 {
          */
         while(true){
             node = node.next;
-            if(node.next != null){
+            if(node != null){
                 node_1.next = node;
                 node_1 = node;
             }else {
                 break;
             }
             node = node.next;
-            if(node.next != null){
+            if(node != null){
                 node_2.next = node;
                 node_2 = node;
             }else {
@@ -43,30 +43,49 @@ public class Problem_24 {
          * 3. 将俩个链表合成1个
          */
         ListNode result_head = head_2;
-        node = result_head;
-        node_1 = head_1;
-        node_2 = head_2;
+        node = head_1;
+        node_1 = head_1.next;
+        node_2 = head_2.next;
         while(true){
-            node_2 = node_2.next;
             if(node_2 != null){
                 node.next = node_2;
                 node = node_2;
+                node_2 = node_2.next;
             }else {
                 break;
             }
-            node_1 = node_1.next;
             if(node_1 != null){
                 node.next = node_1;
                 node = node_1;
+                node_1 = node_1.next;
             }else {
                 break;
             }
         }
+        head_2.next = head_1;
+
         /**
          * 4. 将链表1剩余的一个拼上
          */
-        if(node_1  != null && node_1.next != null){
-            node.next =
+        if(node_1 != null){
+            node.next = node_1;
+            node = node_1;
         }
+        /**
+         * 5. 尾节点置为空
+         */
+        node.next = null;
+        return result_head;
     }
+    public static void main(String[] args){
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next = new ListNode(6);
+
+        new Problem_24().swapPairs(l1);
+    }
+
 }
